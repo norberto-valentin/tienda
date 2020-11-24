270d4f18cf5eaab4624f99a1e45941db9da8df93
norberto-valentin <jesusnorbertoantonio@gmail.com>
2020-11-23 20:01:16 -0600

    version 1

---
+++ js/productos.js
@@ -0,0 +1,10 @@
+function cerrar(){
+  document.getElementById('visual').style.display="block"
+  document.getElementById('muestra').style.display="none"
+  document.getElementById('listado').style.display="flex"
+}
+
+function mostrar(){
+  document.getElementById('muestra').style.display="flex";
+  document.getElementById('listado').style.display="none"
+}
